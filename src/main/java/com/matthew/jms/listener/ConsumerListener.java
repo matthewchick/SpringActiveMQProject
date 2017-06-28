266db69444c2656da90d411c190616a3b7ecbb51
Matthew Chick <chikmatthew@gmail.com>
2017-06-28 21:06:02 +1000

    Add JUnit Test under src/test/java using Java Unit Case, Use testOnMessage() and assertNull()

--- src/main/java/com/matthew/jms/listener/ConsumerListener.java
+++ src/main/java/com/matthew/jms/listener/ConsumerListener.java
@@ -1,10 +1,15 @@
+// http://www.codejava.net/frameworks/spring/creating-a-spring-mvc-project-using-maven-and-eclipse-in-one-minute
 package com.matthew.jms.listener;
 
+/* JMS is not a MOM (Message-oriented middleware) 
+ * JMS API can connect different provider, ActiveMQ, TIBCO, WebsphereMQ, SonicMQ
+ */
 import javax.jms.Message;
 import javax.jms.MessageListener;
 
 import org.springframework.stereotype.Component;
 
+//JMS consumer - a client application that receives and processes JMS messages
 @Component
 public class ConsumerListener implements MessageListener {
 
